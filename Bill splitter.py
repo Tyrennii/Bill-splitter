10b19ee507620766707db017435f0a7ba876b795
Archer <lan0128@bssc.edu.au>
2026-08-13 12:53:29 +1000

    Enhance bill splitting with tip calculation
    Added tip percentage input and updated split_bill function to include tip calculation.

--- Bill splitter.py
+++ Bill splitter.py
@@ -1,11 +1,13 @@
-total = float(input("Please enter the total: $"))
+total = float(input("Please enter the total: $ "))
 
 
 num_people = int(input("Please enter the amount of people you would like to split the bill with: ")) ## totally couldve used a float here but apparently its better for other people reading tge code to have it using the right value.
 
-def split_bill(total, num_people):
-    result = total / num_people
+tip_percentage = float(input("please enter the % you would like to tip: % "))
+
+def split_bill(total, num_people, tip_percentage):
+    result = total * (tip_percentage / 100) / num_people
     return(result)
-answer = split_bill(total, num_people)
+answer = split_bill(total, num_people, tip_percentage)
 print(f"${answer:.2f}")
 
